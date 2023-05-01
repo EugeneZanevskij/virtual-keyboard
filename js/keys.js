0ce6941c4cf192bc6b3230d8a1c197a90dc123c8
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-05-01 14:39:01 +0300

    feat: add keys for ru

--- js/keys.js
+++ js/keys.js
@@ -237,6 +237,7 @@ export const keys = {
     {
       event: "Slash",
       key: "/",
+      shift: "?",
     },
     {
       event: "ArrowUp",
@@ -285,5 +286,283 @@ export const keys = {
       key: "Ctrl",
     }
   ]
-]
+],
+  ru: [
+  [
+    {
+      event: "Backquote",
+      key: "ё",
+      shift: "Ё",
+    },
+    {
+      event: "Digit1",
+      key: "1",
+      shift: "!",
+    },
+    {
+      event: "Digit2",
+      key: "2",
+      shift: "\"",
+    },
+    {
+      event: "Digit3",
+      key: "3",
+      shift: "№",
+    },
+    {
+      event: "Digit4",
+      key: "4",
+      shift: ";",
+    },
+    {
+      event: "Digit5",
+      key: "5",
+      shift: "%",
+    },
+    {
+      event: "Digit6",
+      key: "6",
+      shift: ":",
+    },
+    {
+      event: "Digit7",
+      key: "7",
+      shift: "?",
+    },
+    {
+      event: "Digit8",
+      key: "8",
+      shift: "*",
+    },
+    {
+      event: "Digit9",
+      key: "9",
+      shift: "(",
+    },
+    {
+      event: "Digit0",
+      key: "0",
+      shift: ")",
+    },
+    {
+      event: "Minus",
+      key: "-", 
+      shift: "_",
+    },
+    {
+      event: "Equal",
+      key: "=",
+      shift: "+",
+    },
+    {
+      event: "Backspace",
+      key: "Backspace",
+    }
+  ],
+  [
+    {
+      event: "Tab",
+      key: "Tab",
+    },
+    {
+      event: "KeyQ",
+      key: "й",
+    },
+    {
+      event: "KeyW",
+      key: "ц",
+    },
+    {
+      event: "KeyE",
+      key: "у",
+    },
+    {
+      event: "KeyR",
+      key: "к",
+    },
+    {
+      event: "KeyT",
+      key: "е",
+    },
+    {
+      event: "KeyY",
+      key: "н",
+    },
+    {
+      event: "KeyU",
+      key: "г",
+    },
+    {
+      event: "KeyI",
+      key: "ш",
+    },
+    {
+      event: "KeyO",
+      key: "щ",
+    },
+    {
+      event: "KeyP",
+      key: "з",
+    },
+    {
+      event: "BracketLeft",
+      key: "х",
+    },
+    {
+      event: "BracketRight",
+      key: "ъ",
+    },
+    {
+      event: "Delete",
+      key: "Del",
+    }
+  ],
+  [
+    {
+      event: "CapsLock",
+    key: "CapsLock",
+    },
+    {
+      event: "KeyA",
+      key: "ф",
+    },
+    {
+      event: "KeyS",
+      key: "ы",
+    },
+    {
+      event: "KeyD",
+      key: "в",
+    },
+    {
+      event: "KeyF",
+      key: "а",
+    },
+    {
+      event: "KeyG",
+      key: "п",
+    },
+    {
+      event: "KeyH",
+      key: "р",
+    },
+    {
+      event: "KeyJ",
+      key: "о",
+    },
+    {
+      event: "KeyK",
+      key: "л",
+    },
+    {
+      event: "KeyL",
+      key: "д",
+    },
+    {
+      event: "Semicolon",
+      key: "ж",
+    },
+    {
+      event: "Quote",
+      key: "э",
+    },
+    {
+      event: "Enter",
+      key: "Enter",
+    }
+  ],
+  [
+    {
+      event: "ShiftLeft",
+      key: "Shift",
+    },
+    {
+      event: "KeyZ",
+      key: "я",
+    },
+    {
+      event: "KeyX",
+      key: "ч",
+    },
+    {
+      event: "KeyC",
+      key: "с",
+    },
+    {
+      event: "KeyV",
+      key: "м",
+    },
+    {
+      event: "KeyB",
+      key: "и",
+    },
+    {
+      event: "KeyN",
+      key: "т",
+    },
+    {
+      event: "KeyM",
+      key: "ь",
+    },
+    {
+      event: "Comma",
+      key: "б",
+    },
+    {
+      event: "Period",
+      key: "ю",
+    },
+    {
+      event: "Slash",
+      key: ".",
+      shift: ",",
+    },
+    {
+      event: "ArrowUp",
+      key: "▲",
+    },
+    {
+      event: "ShiftRight",
+      key: "Shift",
+    }
+  ],
+  [
+    {
+      event: "ControlLeft",
+      key: "Ctrl",
+    },
+    {
+      event: "MetaLeft",
+      key: "Win",
+    },
+    {
+      event: "AltLeft",
+      key: "Alt",
+    },
+    {
+      event: "Space",
+      key: " ",
+    },
+    {
+      event: "AltRight",
+      key: "Alt",
+    },
+    {
+      event: "ArrowLeft",
+      key: "◄",
+    },
+    {
+      event: "ArrowDown",
+      key: "▼",
+    },
+    {
+      event: "ArrowRight",
+      key: "►",
+    },
+    {
+      event: "ControlRight",
+      key: "Ctrl",
+    }
+  ]
+  ]
 };
